4b3ac867805d337119607f79ba5775bffa203a9c
Andre Herman <amelco.herman@gmail.com>
2019-07-03 18:40:38 -0300

    Add API base URL for deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333', 
+    // baseURL: 'http://localhost:3333',    // development
+    baseURL: 'https://morning-falls-78292.herokuapp.com/',  // deploy 
 });
 
 export default api;
